99406a876ed1229de842edbca6429ca00e7dc2a5
nilphym <nilphym@gmail.com>
2021-10-06 20:17:45 +0200

    Add css baseline and theme provider to storybook

--- Frontend/.storybook/preview.js
+++ Frontend/.storybook/preview.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import theme from '../src/utils/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  )
+];
