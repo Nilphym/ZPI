7e721df66362ed3a8f91172faf0ea0084b71458e
Elentiya <hornik.daria@gmail.com>
2021-10-17 13:34:37 +0200

    nowe endpointów: dodanie testu i wyświetlenie testu

---
+++ Backend/Funtest/Services/Interfaces/ITestService.cs
@@ -0,0 +1,13 @@
+﻿using Funtest.TransferObject.Test.Requests;
+using Funtest.TransferObject.Test.Response;
+using System;
+using System.Threading.Tasks;
+
+namespace Funtest.Services.Interfaces
+{
+    public interface ITestService
+    {
+        Task<bool> AddTest(AddTestRequest test);
+        GetTestResponse GetTestById(Guid id);
+    }
+}
---
+++ Backend/Funtest/TransferObject/TestProcedure/Requests/AddTestProcedureRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Funtest.TransferObject.TestProcedure.Requests
+{
+    public class AddTestProcedureRequest
+    {
+        public Guid? Id { get; set; }
+
+        public string Result { get; set; }
+
+        public Guid? TestCaseId { get; set; }
+    }
+}
