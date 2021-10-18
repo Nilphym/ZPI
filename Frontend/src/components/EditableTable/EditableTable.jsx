943873571930e0c7b1268387a0d0a0838b07e66c
Michal Kaja <m.kaja@wp.pl>
2021-10-18 20:02:12 +0200

    fast

--- Frontend/src/components/EditableTable/EditableTable.jsx
+++ Frontend/src/components/EditableTable/EditableTable.jsx
@@ -1,33 +1,102 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
+import CreateIcon from '@mui/icons-material/Create';
 import DeleteIcon from '@mui/icons-material/Delete';
 import TableItem from './TableItem';
 
 const MAX_ROWS_NUMBER = 11;
 const MAX_COLUMNS_NUMBER = 11;
 
+const demo = {
+  tableName: 'aaa',
+  RowName1: '',
+  Data1: ['ac', '1', '2', 'b', '3', 'c'],
+  RowName2: 'Ble ble',
+  Data2: ['x', 'y', 'z', 't', 'r', 'w']
+};
+
+const processData = (data) => {
+  const originalDataKeys = Object.keys(data);
+  const dataKeys = originalDataKeys.slice(1);
+  const processedData = [];
+  let array = [];
+  dataKeys.forEach((key) => {
+    if (key.toString().includes('RowName')) {
+      array = [data[key]];
+    }
+    if (key.toString().includes('Data')) {
+      array = [...array, ...data[key]];
+      processedData.push([...array]);
+      array.length = 0;
+    }
+  });
+
+  return processedData;
+};
+
+
+const prepareOutputData = () => {
+
+};
+
 // TODO: Add control from outer form !!!
-const EditableTable = ({
-  rowsNumber: defaultRowsNumber,
-  columnsNumber: defaultColumnsNumber,
-  name,
-  disabled,
-  deleteTable
-}) => {
-  const { control } = useForm();
-  const [rowsNumber, setRowsNumber] = useState(defaultRowsNumber + 1);
-  const [columnsNumber, setColumnsNumber] = useState(defaultColumnsNumber + 1);
+const EditableTable = ({ name, disabled, deleteTable, data }) => {
+  const { control: tableControl, getValues, setValue } = useForm();
+
+  const defaultData = processData(demo);
+  const [rowsNumber, setRowsNumber] = useState(defaultData.length);
+  const [columnsNumber, setColumnsNumber] = useState(defaultData[0].length);
+  const [currentData, setCurrentData] = useState(defaultData);
+  const [isEditing, setIsEditing] = useState(false);
+
+
+  const addRow = () => {
+    setRowsNumber((number) => number + 1);
+    const copyData = [...currentData];    
+    copyData.push([...Array.from(Array(copyData[copyData.length - 1].length).keys())].fill(''));
+    setCurrentData(copyData);
+  };
+
+  const addColumn = () => {
+    setColumnsNumber((number) => number + 1);
+    let copyData = [...currentData];
+    copyData = copyData.map(row => [...row, '']);
+    setCurrentData(copyData);
+  };
+  
+  const deleteRow = () => {
+    setRowsNumber((number) => number - 1);
+    let copyData = [...currentData];
+    copyData = copyData.slice(0, copyData.length - 1);
+    setCurrentData(copyData);
+  };
+  
+  const deleteColumn = () => {
+    setColumnsNumber((number) => number - 1);
+    let copyData = [...currentData];
+    copyData = copyData.map((row) => row.slice(0, row.length-1));
+    setCurrentData(copyData);
+  };
+  
+  useEffect(() => {
+    setValue('Genowefa', { a: '1', b: '2' });
+    console.log('rerender');
+  }, []);
 
   return (
-    <Box sx={{
-      marginTop: '1.25rem'
-    }}>
-      <Typography sx={{
-        fontWeight: '700'
-      }}>
-        {`Table name: ${name}`}
+    <Box
+      sx={{
+        marginTop: '1.25rem'
+      }}
+    >
+      <Typography
+        sx={{
+          fontWeight: '700'
+        }}
+      >
+        {`Table name: ${demo.tableName}`}
       </Typography>
       <Box
         sx={{
@@ -36,23 +105,23 @@ const EditableTable = ({
           marginTop: '0.625rem'
         }}
       >
-        {!disabled && rowsNumber < MAX_ROWS_NUMBER && (
-          <Button onClick={() => setRowsNumber((number) => number + 1)}>Add row</Button>
+        {!disabled && isEditing && rowsNumber < MAX_ROWS_NUMBER && (
+          <Button onClick={() => addRow()}>Add row</Button>
         )}
-        {!disabled && columnsNumber < MAX_COLUMNS_NUMBER && (
-          <Button onClick={() => setColumnsNumber((number) => number + 1)}>Add column</Button>
+        {!disabled && isEditing && columnsNumber < MAX_COLUMNS_NUMBER && (
+          <Button onClick={() => addColumn()}>Add column</Button>
         )}
-        {!disabled && rowsNumber > 2 && (
-          <Button onClick={() => setRowsNumber((number) => number - 1)}>Delete row</Button>
+        {!disabled && isEditing && rowsNumber > 2 && (
+          <Button onClick={() => deleteRow()}>Delete row</Button>
         )}
-        {!disabled && columnsNumber > 2 && (
-          <Button onClick={() => setColumnsNumber((number) => number - 1)}>Delete column</Button>
+        {!disabled && isEditing && columnsNumber > 2 && (
+          <Button onClick={() => deleteColumn()}>Delete column</Button>
         )}
         {!disabled && (
           <DeleteIcon
             sx={{
               position: 'absolute',
-              top: '1vh',
+              top: '-6vh',
               right: '1vw',
               border: '1px solid black',
               borderRadius: '50%',
@@ -64,43 +133,82 @@ const EditableTable = ({
             onClick={() => deleteTable()}
           />
         )}
+        {!disabled && !isEditing && (
+          <CreateIcon
+            sx={{
+              position: 'absolute',
+              top: '-6vh',
+              right: '4vw',
+              border: '1px solid black',
+              borderRadius: '50%',
+              padding: '2px',
+              '&:hover': {
+                cursor: 'pointer'
+              }
+            }}
+            onClick={() => setIsEditing(true)}
+          />
+        )}
         <Box
           sx={{
             width: '100%'
           }}
         >
-          {[...Array.from(Array(rowsNumber).keys())].map((rowNumber) => (
-            <Box
-              key={`row-${rowNumber}`}
-              sx={{
-                display: 'flex'
-              }}
-            >
-              {[...Array.from(Array(columnsNumber).keys())].map((columnNumber) => (
-                <TableItem
-                  key={`${rowNumber}-${columnNumber}`}
-                  name={name}
-                  row={rowNumber}
-                  column={columnNumber}
-                  control={control}
-                  disabled={disabled}
-                />
-              ))}
-            </Box>
-          ))}
+          <Box
+            sx={{
+              width: '100%'
+            }}
+          >
+            {currentData.map((row, rowIndex) => (
+              <Box
+                key={`row-${row[0]}`}
+                sx={{
+                  display: 'flex'
+                }}
+              >
+                {row.map((item, columnIndex) => (
+                  <TableItem
+                    key={`${row[0]}-${item}`}
+                    name={demo.tableName}
+                    row={rowIndex}
+                    column={columnIndex}
+                    control={tableControl}
+                    disabled={disabled}
+                    defaultValue={item}
+                  />
+                ))}
+              </Box>
+            ))}
+          </Box>
         </Box>
       </Box>
+      <Button sx={{
+        position: 'absolute',
+        left: '50vw'
+      }} onClick={() => console.log(getValues())}>
+        Get Values
+      </Button>
+      {isEditing && (
+        <Button
+          variant="outlined"
+          sx={{ marginTop: '1.5rem' }}
+          onClick={() => {
+            setIsEditing(false);
+          }}
+        >
+          Save Table
+        </Button>
+      )}
     </Box>
   );
 };
 
 EditableTable.propTypes = {
   name: PropTypes.string.isRequired,
-  rowsNumber: PropTypes.number.isRequired,
-  columnsNumber: PropTypes.number.isRequired,
   // control: PropTypes.object.isRequired,
   disabled: PropTypes.bool.isRequired,
-  deleteTable: PropTypes.func.isRequired
+  deleteTable: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
 };
 
 export default EditableTable;
