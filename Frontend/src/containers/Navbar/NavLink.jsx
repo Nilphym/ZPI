61e98243c62d206c9dd79612ba05de6ddb2243e9
Nilphym <nilphym@gmail.com>
2021-10-25 17:30:42 +0200

    Create better navigation

---
+++ Frontend/src/containers/Navbar/NavLink.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import { blue, grey } from '@mui/material/colors';
+import { styled } from '@mui/system';
+
+const StyledLink = styled(Link, {
+  shouldForwardProp: (prop) => prop !== 'active' || prop !== 'compact'
+})(({ active, compact, theme }) => ({
+  padding: '1rem 2rem',
+  display: 'flex',
+  color: theme.palette.primary.dark,
+  textDecoration: 'none',
+  ...(compact && {
+    padding: '1.5rem 0',
+    justifyContent: 'center'
+  }),
+  ...(!active && {
+    '&:hover': {
+      backgroundColor: grey[50]
+    }
+  }),
+  ...(active && {
+    backgroundColor: blue[50]
+  })
+}));
+
+const NavLink = ({ pathname, icon, text, destination, compact }) => {
+  const regex = new RegExp(destination);
+
+  return (
+    <StyledLink
+      compact={compact ? 'compact' : null}
+      active={regex.test(pathname) ? 'active' : null}
+      to={destination}
+    >
+      {icon}
+      {!compact && <Typography sx={{ paddingLeft: '1rem' }}>{text}</Typography>}
+    </StyledLink>
+  );
+};
+
+NavLink.propTypes = {
+  pathname: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  text: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+  compact: PropTypes.bool.isRequired
+};
+
+export default NavLink;
--- Frontend/src/containers/Navbar/Navbar.jsx
+++ Frontend/src/containers/Navbar/Navbar.jsx
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import {
-  Box,
-  Typography,
-  Avatar,
-  IconButton,
-  Slide,
-  Fade,
-  useMediaQuery,
-  Button,
-  Menu,
-  MenuItem
-} from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { Box, IconButton, Slide, Fade, useMediaQuery, Paper, Divider } from '@mui/material';
 import {
   Login,
   FormatListBulleted,
@@ -23,10 +12,14 @@ import {
   Menu as MenuIcon,
   Person
 } from '@mui/icons-material';
-import { blue, grey } from '@mui/material/colors';
+import { grey } from '@mui/material/colors';
 import { styled, useTheme } from '@mui/system';
 
+import NavProfile from './NavProfile';
+import NavLink from './NavLink';
+import NavMultiLink from './NavMultiLink';
 import logo from '../../assets/logo/logo2.png';
+import logoIcon from '../../assets/logo/icon.png';
 
 const icons = {
   dashboard: <BarChart />,
@@ -40,146 +33,24 @@ const Logo = styled('img')({
   width: '8rem'
 });
 
-const StyledLink = styled(Link, {
-  shouldForwardProp: (prop) => prop !== 'active'
-})(({ active, theme }) => ({
-  width: '100%',
-  padding: '1rem 2rem',
-  display: 'flex',
-  alignItems: 'center',
-  color: theme.palette.primary.dark,
-  textDecoration: 'none',
-  ...(!active && {
-    '&:hover': {
-      backgroundColor: grey[50]
-    }
-  }),
-  ...(active && {
-    backgroundColor: blue[50]
-  })
-}));
-
-const StyledButton = styled(Button, {
-  shouldForwardProp: (prop) => prop !== 'active'
-})(({ active, theme }) => ({
-  width: '100%',
-  padding: '1rem 2rem',
-  display: 'flex',
-  alignItems: 'center',
-  color: theme.palette.primary.dark,
-  textDecoration: 'none',
-  ...(!active && {
-    '&:hover': {
-      backgroundColor: grey[50]
-    }
-  }),
-  ...(active && {
-    backgroundColor: blue[50]
-  }),
-  textAlign: 'left',
-  textTransform: 'none'
-}));
-
-const NavbarItem = ({ pathname, link: { icon, text, destination } }) => {
-  const regex = new RegExp(destination);
-
-  return (
-    <StyledLink active={regex.test(pathname)} to={destination}>
-      {icons[icon]}
-      <Typography sx={{ width: '100%', paddingLeft: '1rem' }}>{text}</Typography>
-    </StyledLink>
-  );
-};
-
-NavbarItem.propTypes = {
-  pathname: PropTypes.string.isRequired,
-  link: PropTypes.shape({
-    icon: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    destination: PropTypes.string.isRequired
-  }).isRequired
-};
-
-const NavbarMenu = ({ name, icon, links }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const navigate = useNavigate();
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleMenuItem = (destination) => {
-    setAnchorEl(null);
-    navigate(destination);
-  };
-
-  return (
-    <>
-      <StyledButton onClick={handleClick}>
-        {icons[icon]}
-        <Typography sx={{ width: '100%', paddingLeft: '1rem' }}>{name}</Typography>
-      </StyledButton>
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-        {links.map((link) => (
-          <MenuItem key={link.text} onClick={() => handleMenuItem(link.destination)}>
-            {link.text}
-          </MenuItem>
-        ))}
-      </Menu>
-    </>
-  );
-};
-
-NavbarMenu.propTypes = {
-  name: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
-  links: PropTypes.array.isRequired
-};
-
-const Profile = ({ avatar, name }) => {
-  return (
-    <Box
-      sx={{
-        alignSelf: 'center',
-        justifySelf: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}
-    >
-      <Avatar sx={{ backgroundColor: 'primary.dark', marginBottom: '0.5rem' }} src={avatar}>
-        {name.at(0)}
-      </Avatar>
-      <Typography sx={{ color: 'primary.dark' }}>{name}</Typography>
-    </Box>
-  );
-};
-
-Profile.propTypes = {
-  avatar: PropTypes.string,
-  name: PropTypes.string.isRequired
-};
-
-Profile.defaultProps = {
-  avatar: null
-};
+const SmallLogo = styled('img')({
+  width: '2rem',
+  margin: '2.7rem 0'
+});
 
 export const Navbar = ({ links }) => {
-  const { pathname } = useLocation();
-  const theme = useTheme();
-  const name = useSelector((state) => state.auth.token.name);
-  const largeMedia = useMediaQuery(theme.breakpoints.up('lg'));
   const [open, setOpen] = useState(true);
+  const { pathname } = useLocation();
+  const name = useSelector((state) => state.auth.token.name);
+  const theme = useTheme();
+  const largeMedia = useMediaQuery(theme.breakpoints.up('lg'));
 
   const toggleOpen = () => {
     setOpen((open) => !open);
   };
 
   useEffect(() => {
-    if ((largeMedia && !open) || (!largeMedia && open)) toggleOpen();
+    if (largeMedia && !open) toggleOpen();
   }, [largeMedia]);
 
   return (
@@ -188,14 +59,15 @@ export const Navbar = ({ links }) => {
         <Box
           sx={{
             height: '100vh',
-            width: '18.8rem',
+            minWidth: '15rem',
+            maxWidth: '15rem',
             display: 'grid',
             gridTemplateRows: '7rem 1fr 7rem',
             borderRight: `2px solid ${grey[100]}`,
             backgroundColor: 'white',
             position: 'sticky',
             top: 0,
-            zIndex: 1
+            zIndex: 2
           }}
         >
           <Box
@@ -207,34 +79,85 @@ export const Navbar = ({ links }) => {
             }}
           >
             <Logo src={logo} alt="logo" />
-            <IconButton onClick={toggleOpen}>
-              <MenuOpen sx={{ color: 'primary.dark' }} />
-            </IconButton>
+            {!largeMedia && (
+              <IconButton onClick={toggleOpen}>
+                <MenuOpen sx={{ color: 'primary.dark' }} />
+              </IconButton>
+            )}
           </Box>
           <Box sx={{ alignSelf: 'center' }}>
             {links.map((link) =>
               link.name ? (
-                <NavbarMenu key={link.name} name={link.name} icon={link.icon} links={link.links} />
+                <NavMultiLink
+                  key={link.name}
+                  name={link.name}
+                  icon={icons[link.icon]}
+                  links={link.links}
+                  compact={!open}
+                />
               ) : (
-                <NavbarItem key={link.destination} pathname={pathname} link={link} />
+                <NavLink
+                  key={link.destination}
+                  pathname={pathname}
+                  icon={icons[link.icon]}
+                  text={link.text}
+                  destination={link.destination}
+                  compact={!open}
+                />
               )
             )}
           </Box>
-          <Profile name={name} />
+          <NavProfile compact={!open} name={name} />
         </Box>
       </Slide>
       <Slide direction="right" in={!open} mountOnEnter unmountOnExit>
-        <Box
+        <Paper
           sx={{
-            position: 'absolute',
-            top: '2.3rem',
-            left: '1rem'
+            height: '100vh',
+            minWidth: '4rem',
+            maxWidth: '4rem',
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+            backgroundColor: 'white',
+            position: 'sticky',
+            top: 0,
+            zIndex: 2
           }}
+          elevation={2}
+          component={Box}
         >
-          <IconButton onClick={toggleOpen}>
+          <SmallLogo src={logoIcon} alt="logo" />
+          <Divider sx={{ alignSelf: 'stretch' }} variant="middle" />
+          <IconButton sx={{ margin: '1rem 0' }} onClick={toggleOpen}>
             <MenuIcon sx={{ color: 'primary.dark' }} />
           </IconButton>
-        </Box>
+          <Divider sx={{ alignSelf: 'stretch' }} variant="middle" />
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            {links.map((link) =>
+              link.name ? (
+                <NavMultiLink
+                  key={link.name}
+                  name={link.name}
+                  icon={icons[link.icon]}
+                  links={link.links}
+                  compact={!open}
+                />
+              ) : (
+                <NavLink
+                  key={link.destination}
+                  icon={icons[link.icon]}
+                  pathname={pathname}
+                  text={link.text}
+                  destination={link.destination}
+                  compact={!open}
+                />
+              )
+            )}
+          </Box>
+          <Divider sx={{ alignSelf: 'stretch' }} variant="middle" />
+          <NavProfile compact={!open} name={name} />
+        </Paper>
       </Slide>
       <Fade in={!largeMedia && open} mountOnEnter unmountOnExit>
         <Box
@@ -244,7 +167,7 @@ export const Navbar = ({ links }) => {
             width: '100vw',
             backgroundColor: 'rgba(0,0,0,0.2)',
             top: 0,
-            zIndex: 0
+            zIndex: 1
           }}
           onClick={toggleOpen}
         />
