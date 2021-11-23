9f3be0659d43857f8cb1b4e465708d4c394feb79
Nilphym <nilphym@gmail.com>
2021-11-23 10:24:25 +0100

    Connect dashboard to server

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -65,14 +65,8 @@ const Layout = () => {
         <Box sx={{ display: 'grid', gridTemplateColumns: 'min-content 1fr' }}>
           <Navbar
             links={[
-              {
-                icon: 'dashboard',
-                name: 'Dashboard',
-                links: [
-                  { text: 'Dashboard', destination: '/dashboard' },
-                  { text: 'Raports', destination: '/raports' }
-                ]
-              },
+              { icon: 'dashboard', text: 'Dashboard', destination: '/dashboard' },
+              { icon: 'tests', text: 'Test Plans', destination: '/testPlans' },
               { icon: 'bugs', text: 'Bugs', destination: '/bugs' },
               { icon: 'profile', text: 'Profile', destination: '/profile' },
               { icon: 'logout', text: 'Logout', destination: '/logout' }
--- Frontend/src/containers/Dashboard/Dashboard.jsx
+++ Frontend/src/containers/Dashboard/Dashboard.jsx
@@ -1,68 +1,66 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Box, useMediaQuery, CircularProgress } from '@mui/material';
 
 import { Statistic, PieChart, PercentageChart, DoughnutChart } from '../../components';
+import { getRaport } from '../../redux/store';
 
 export const Dashboard = () => {
-  const args = {
-    daysFromStart: 54,
-    testersNumber: 13,
-    devsNumber: 10,
-    testSuitesNumber: 21,
-    testSuitesByNames: [
-      { name: 'Login', value: 13 },
-      { name: 'Add new product', value: 18 },
-      { name: 'Shop', value: 4 },
-      { name: 'Pay bills', value: 7 }
-    ]
-  };
+  const { loading, data: raportData } = useSelector((state) => state.raports);
+  const smallMedia = useMediaQuery('(max-width: 1100px)');
+  const dispatch = useDispatch();
 
-  return (
+  useEffect(() => {
+    dispatch(getRaport());
+  }, []);
+
+  return loading ? (
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <CircularProgress />
+    </Box>
+  ) : (
     <Box
       sx={{
         display: 'grid',
-        gridTemplate: 'repeat(5, 9rem) / repeat(3, max-content)',
+        gridTemplateColumns: smallMedia ? 'repeat(2, max-content)' : 'repeat(3, max-content)',
         gap: '2rem',
         justifyContent: 'center'
       }}
     >
-      <Statistic name="Days from product start" number={args.daysFromStart} icon="time" />
-      <Statistic name="Testers" number={args.testersNumber} icon="person" />
-      <Statistic name="Developers" number={args.devsNumber} icon="person" />
+      <Statistic name="Days from product start" number={raportData.daysFromStart} icon="time" />
+      <Statistic name="Testers" number={raportData.testersNumber} icon="person" />
+      <Statistic name="Developers" number={raportData.devsNumber} icon="person" />
       <PieChart
         sx={{ gridRow: 'span 2' }}
         name="Bugs by impact"
         label="Total bugs"
-        data={[
-          { name: 'High', value: 13 },
-          { name: 'Medium', value: 19 },
-          { name: 'Low', value: 6 }
-        ]}
+        data={raportData.bugsByImpact}
       />
       <PieChart
         sx={{ gridRow: 'span 2' }}
         name="Bugs by priority"
         label="Total bugs"
-        data={[
-          { name: 'High', value: 22 },
-          { name: 'Medium', value: 12 },
-          { name: 'Low', value: 4 }
-        ]}
+        data={raportData.bugsByPriority}
       />
-      <Statistic name="Test suites" number={args.testSuitesNumber} icon="test" />
-      <Statistic name="Rejected bugs" number={5} icon="bug" />
+      <Statistic name="Test suites" number={raportData.testSuitesNumber} icon="test" />
+      <Statistic
+        sx={smallMedia ? { gridRow: 7, gridColumn: 2 } : {}}
+        name="Rejected bugs"
+        number={raportData.bugsRejected}
+        icon="bug"
+      />
       <DoughnutChart
         sx={{ gridColumn: 'span 2', gridRow: 'span 2' }}
         name="Test suites"
         label="Tests"
-        data={args.testSuitesByNames}
+        data={raportData.testSuitesByName}
       />
       <PercentageChart
         sx={{ gridRow: 'span 2' }}
         name="Bugs fixed"
         completedLabel="Fixed"
-        all={52}
-        completed={17}
+        all={raportData.bugsAll}
+        completed={raportData.bugsFixed}
       />
     </Box>
   );
