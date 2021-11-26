775e10d0a4df50e7c6a5f55bac3e262a0f14fe40
Elentiya <hornik.daria@gmail.com>
2021-11-26 22:54:14 +0100

    Added changes from long time

---
+++ Backend/Funtest/TransferObject/Test/Response/GetTestWithProcedureAndCaseTestResponse.cs
@@ -0,0 +1,24 @@
+﻿using Funtest.TransferObject.TestCase.Responses;
+using Funtest.TransferObject.TestProcedure.Responses;
+using Funtest.TransferObject.TestSuite.Responses;
+using System;
+
+namespace Funtest.TransferObject.Test.Response
+{
+    public class GetTestWithProcedureAndCaseTestResponse
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int ExecutionCounter { get; set; }
+
+        public string Name { get; set; }
+
+        public GetTestSuiteResponse TestSuite { get; set; }
+
+        public GetResultTestProcedureResponse TestProcedure { get; set; }
+
+        public GetTestCaseWithTabelFormatResponse TestCase { get; set; }
+    }
+}
