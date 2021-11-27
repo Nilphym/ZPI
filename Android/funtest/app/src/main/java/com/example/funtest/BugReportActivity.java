6b92e85d169411ffc656538cba441a9f7ec3884c
MaciejChrzastek <maciek.chrzastek@gmail.com>
2021-11-27 22:45:04 +0100

    finished adding api connection and fixed many bugs

--- Android/funtest/app/src/main/java/com/example/funtest/BugReportActivity.java
+++ Android/funtest/app/src/main/java/com/example/funtest/BugReportActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,16 +23,40 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.funtest.objects.TestCase;
+import com.example.funtest.objects.TestProcedure;
 import com.example.funtest.utils.ListViewUtils;
+import com.example.funtest.utils.VolleyMultipartRequest;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class BugReportActivity extends AppCompatActivity {
 
@@ -36,6 +67,7 @@ public class BugReportActivity extends AppCompatActivity {
     FloatingActionButton fab_report, fab_cancel;
 
     ArrayList<Bitmap> attachmentsBitMap;
+    ArrayList<String> attachmentsNames;
     public static final int PICK_IMAGE = 1;
 
     ArrayList<String> labels_type;
@@ -45,6 +77,11 @@ public class BugReportActivity extends AppCompatActivity {
     final Calendar myCalendar = Calendar.getInstance();
 
     private String testId;
+    private String stepId;
+    private String finalDate;
+
+    private ProgressDialog pDialog;
+
 
 
     @Override
@@ -76,6 +113,7 @@ public class BugReportActivity extends AppCompatActivity {
 
         //initialize bitmap list
         attachmentsBitMap = new ArrayList<>();
+        attachmentsNames = new ArrayList<>();
 
         //initialize calendar date picker
 
@@ -126,8 +164,24 @@ public class BugReportActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(attachmentsBitMap.size()>0){
                     attachmentsBitMap.remove((attachmentsBitMap.size()-1));
+                    attachmentsNames.remove((attachmentsNames.size()-1));
                     refreshAttachmentsList();
                 }
+            }
+        });
+
+        fab_report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                reportBug();
+            }
+        });
+
+        fab_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 
@@ -156,12 +210,13 @@ public class BugReportActivity extends AppCompatActivity {
 
     private void get_testId() {
 
-        testId = getIntent().getStringExtra("id");
+        testId = getIntent().getStringExtra("testId");
+        stepId = getIntent().getStringExtra("stepId");
 
     }
 
     private void updateDeadlineLabel() {
-        String myFormat = "dd/MM/yy"; //In which you need put here
+        String myFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.UK);
 
         editText_deadline.setText(sdf.format(myCalendar.getTime()));
@@ -179,6 +234,7 @@ public class BugReportActivity extends AppCompatActivity {
                 InputStream inputStream = getApplicationContext().getContentResolver().openInputStream(data.getData());
                 Bitmap selectedImage = BitmapFactory.decodeStream(inputStream);
                 attachmentsBitMap.add(selectedImage);
+                attachmentsNames.add(getFileName(data.getData()));
                 refreshAttachmentsList();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -188,14 +244,345 @@ public class BugReportActivity extends AppCompatActivity {
     }
 
     private void refreshAttachmentsList() {
-        ArrayList<String> attachmentsLabels = new ArrayList<>();
-        for(int i=0;i< attachmentsBitMap.size();i++){
-            attachmentsLabels.add("Attachment "+ (i+1));
-        }
-        ArrayAdapter<String> entryDataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, attachmentsLabels);
+        ArrayAdapter<String> entryDataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, attachmentsNames);
         listView_attachments.setAdapter(entryDataAdapter);
         ListViewUtils.setListViewHeightBasedOnChildren(listView_attachments);
 
+
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    private void reportBug(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = preferences.getString("Token", "");
+        String userId = preferences.getString("userId","");
+
+        if(!token.equalsIgnoreCase("")) {
+
+            pDialog = new ProgressDialog(this);
+            // Showing progress dialog before making http request
+            pDialog.setMessage("Loading...");
+            pDialog.show();
+
+            String url = "https://fun-test-zpi.herokuapp.com/api/Errors";
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            Date c = java.util.Calendar.getInstance().getTime();
+            SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+            finalDate = df1.format(c);
+
+             /*JSONObject jsonBody = new JSONObject();
+            try {
+
+                Date c = java.util.Calendar.getInstance().getTime();
+                SimpleDateFormat df1 = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+                String formattedDate1 = df1.format(c);
+
+                SimpleDateFormat df2 = new SimpleDateFormat("HH:mm:ssZ", Locale.getDefault());
+                String formattedDate2 = df2.format(c);
+
+                String finalDate = formattedDate1+"T"+formattedDate2;
+
+                jsonBody.put("name", editText_name.getText());
+                jsonBody.put("testId", testId);
+                jsonBody.put("reportDate", finalDate);
+                jsonBody.put("description", editText_description.getText());
+                jsonBody.put("deadline", editText_deadline.getText());
+                jsonBody.put("errorImpact", spinner_impact.getSelectedItem().toString());
+                jsonBody.put("errorPriority", spinner_priority.getSelectedItem().toString());
+                jsonBody.put("errorType", spinner_type.getSelectedItem().toString());
+                jsonBody.put("stepId", stepId);
+                jsonBody.put("testerId", userId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            */
+
+            StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("LOG_RESPONSE", response.toString());
+                    if (pDialog != null) {
+                        pDialog.dismiss();
+                        pDialog = null;
+                    }
+
+                    if(attachmentsBitMap.size()>0){
+                        uploadEveryImage(response);
+                    }
+                    Toast.makeText(getApplicationContext(), "Bug Reported", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_RESPONSE", error.toString());
+                    if (pDialog != null) {
+                        pDialog.dismiss();
+                        pDialog = null;
+                    }
+                    if (error == null || error.networkResponse == null) {
+                        return;
+                    }
+
+                    String body;
+                    //get status code here
+                    final String statusCode = String.valueOf(error.networkResponse.statusCode);
+                    //get response body and parse with appropriate encoding
+                    try {
+                        body = new String(error.networkResponse.data,"UTF-8");
+                        Log.e("LOG_ERROR_DATA", body);
+
+                    } catch (UnsupportedEncodingException e) {
+                        // exception
+                    }
+
+                    //do stuff with the body...
+                }
+            }) {
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    HashMap<String, String> params2 = new HashMap<String, String>();
+                    params2.put("name", editText_name.getText().toString());
+                    params2.put("testId", testId);
+                    params2.put("reportDate", finalDate);
+                    params2.put("description", editText_description.getText().toString());
+                    params2.put("deadline", editText_deadline.getText().toString());
+                    params2.put("errorImpact", spinner_impact.getSelectedItem().toString());
+                    params2.put("errorPriority", spinner_priority.getSelectedItem().toString());
+                    params2.put("errorType", spinner_type.getSelectedItem().toString());
+                    params2.put("stepId", stepId);
+                    params2.put("testerId", userId);
+                    return new JSONObject(params2).toString().getBytes();
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json";
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization", "Bearer "+ token);
+                    return params;
+                }
+            };
+
+            /*
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,url,jsonBody, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.i("LOG_RESPONSE", response.toString());
+                    if (pDialog != null) {
+                        pDialog.dismiss();
+                        pDialog = null;
+                    }
+
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_RESPONSE", error.toString());
+                    if (pDialog != null) {
+                        pDialog.dismiss();
+                        pDialog = null;
+                    }
+                }
+            }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization", "Bearer "+ token);
+                    return params;
+                }
+
+            };
+
+             */
+
+            requestQueue.add(sr);
+        }
+
+    }
+
+    private void uploadEveryImage(String bugId){
+        for(int i=0;i<attachmentsBitMap.size();i++){
+            uploadBitmap(attachmentsBitMap.get(i), bugId);
+        }
+    }
+
+    private void uploadBitmap(final Bitmap bitmap, String bugId) {
+
+        String url = "https://api.imgbb.com/1/upload?key=65fb0fa241f570bb88c73e8678430d0d";
+
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, url,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            //Toast.makeText(getApplicationContext(), obj.getString("data"), Toast.LENGTH_SHORT).show();
+                            Log.i("LOG_RESPONSE", obj.getString("data"));
+
+                            JSONObject dataObject = obj.getJSONObject("data");
+                            String imageUrl = dataObject.getString("url");
+                            Log.i("LOG_RESPONSE", imageUrl);
+
+
+                            uploadImageURL(imageUrl, bugId);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("GotError",""+error.getMessage());
+
+                        if (error == null || error.networkResponse == null) {
+                            return;
+                        }
+
+                        String body;
+                        //get status code here
+                        final String statusCode = String.valueOf(error.networkResponse.statusCode);
+                        //get response body and parse with appropriate encoding
+                        try {
+                            body = new String(error.networkResponse.data,"UTF-8");
+                            Log.e("LOG_ERROR_DATA", body);
+
+                        } catch (UnsupportedEncodingException e) {
+                            // exception
+                        }
+
+                        //do stuff with the body...
+                    }
+                }) {
+
+
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("image", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        //adding the request to volley
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void uploadImageURL(String imageUrl, String bugId){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = preferences.getString("Token", "");
+        if(!token.equalsIgnoreCase("")) {
+
+            pDialog = new ProgressDialog(this);
+            // Showing progress dialog before making http request
+            pDialog.setMessage("Loading...");
+            pDialog.show();
+
+
+            String url = "https://fun-test-zpi.herokuapp.com/api/Attachments";
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+
+
+            StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("LOG_RESPONSE", response.toString());
+                    if (pDialog != null) {
+                        pDialog.dismiss();
+                        pDialog = null;
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_RESPONSE", error.toString());
+                    if (pDialog != null) {
+                        pDialog.dismiss();
+                        pDialog = null;
+                    }
+                    if (error == null || error.networkResponse == null) {
+                        return;
+                    }
+
+                    String body;
+                    //get status code here
+                    final String statusCode = String.valueOf(error.networkResponse.statusCode);
+                    //get response body and parse with appropriate encoding
+                    try {
+                        body = new String(error.networkResponse.data,"UTF-8");
+                        Log.e("LOG_ERROR_DATA", body);
+
+                    } catch (UnsupportedEncodingException e) {
+                        // exception
+                    }
+
+                    //do stuff with the body...
+                }
+            }) {
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    HashMap<String, String> params2 = new HashMap<String, String>();
+                    params2.put("imageLink", imageUrl);
+                    params2.put("errorId", bugId.replaceAll("^\"|\"$", ""));
+                    return new JSONObject(params2).toString().getBytes();
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json";
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization", "Bearer "+ token);
+                    return params;
+                }
+            };
+
+            requestQueue.add(sr);
+        }
 
     }
 }
